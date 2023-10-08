c1420ee39bafe59aa8ed8ebff7fd943ac986b94c
vipul <vipulgowda96@gmail.com>
2023-10-07 17:14:00 -0700

    homework-setup1

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,8 @@
-window.addEventListener('load', loadPage);
+window.addEventListener("load", loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("The page has now loaded.");
+  console.log(
+    "Hey 👋, My name is Vipul, and I am currently pursuing my master's in computer science. I am more inclined towards web development and would like to explore new web development frameworks and libraries. Previously, I worked as a software engineer at The Modern Data Company, where we developed data platforms. I am still learning how to craft beer, and my hobbies include going on trails and road trips with friends."
+  );
 }
